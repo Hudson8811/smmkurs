f302c04b27bfa2da39f4b9cb5aaace1703195047
dmitryboz21 <dmitryboz21@yandex.com>
2021-02-25 16:06:57 +0300

    123

--- src/js/main2.js
+++ src/js/main2.js
@@ -1,5 +1,8 @@
-$(function() {
-	$('.js-cwprog-open-sub').click(function(){
+//= libs/jquery.mCustomScrollbar.concat.min.js
+
+
+$(function () {
+	$('.js-cwprog-open-sub').click(function () {
 		$(this).parent().toggleClass('cwprog-titem-name--active')
 			.siblings('.cwprog-titem-sub').slideToggle(400)
 	});
@@ -18,4 +21,12 @@ $(function() {
 		prevArrow: '<div class="cwfeedback-arrow cwfeedback-arrow--prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 443.52 443.52"><path d="M143.492 221.863L336.226 29.129c6.663-6.664 6.663-17.468 0-24.132-6.665-6.662-17.468-6.662-24.132 0l-204.8 204.8c-6.662 6.664-6.662 17.468 0 24.132l204.8 204.8c6.78 6.548 17.584 6.36 24.132-.42 6.387-6.614 6.387-17.099 0-23.712L143.492 221.863z"/></svg></div>',
 		nextArrow: '<div class="cwfeedback-arrow cwfeedback-arrow--next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 443.52 443.52"><path d="M143.492 221.863L336.226 29.129c6.663-6.664 6.663-17.468 0-24.132-6.665-6.662-17.468-6.662-24.132 0l-204.8 204.8c-6.662 6.664-6.662 17.468 0 24.132l204.8 204.8c6.78 6.548 17.584 6.36 24.132-.42 6.387-6.614 6.387-17.099 0-23.712L143.492 221.863z"/></svg></div>'
 	});
+
+	if ($(".js-ylearn-hor-scroll").length > 0) {
+		$(".js-ylearn-hor-scroll").mCustomScrollbar({
+			axis:"x",
+			theme:"ylearn ",
+			mouseWheel:{ enable: false }
+		});
+	}
 });
